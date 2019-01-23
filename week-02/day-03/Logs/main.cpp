37bbb44a2691b4ac39c0d71aeec1ebff7204f8d9
Adam Kudar <adam.kudar@gmail.com>
2019-01-23 17:31:03 +0100

    Log - unfinished

---
+++ week-02/day-03/Logs/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Read all data from 'log.txt'.
+// Each line represents a log message from a web server
+// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns the GET / POST request ratio.
+
+std::string* uniqueIP(std::string array[], int size);
+void ratio();
+
+int main() {
+
+    int size = 0;
+    int size2 = 0;
+    std::ifstream readFile;
+    std::string line;
+    readFile.open("C:/Users/ASUS/Desktop/greenfox/Log.txt");
+
+    while (getline(readFile, line)) {
+
+        size++;
+    }
+
+    readFile.close();
+
+    std::string bigArray[size];
+
+    readFile.open("C:/Users/ASUS/Desktop/greenfox/Log.txt");
+
+    for (int i = 0; i < size; ++i) {
+
+        std::string tempLine;
+        std::string tempIP = "";
+        getline(readFile, tempLine);
+
+        for (int j = 27; j < 38; ++j) {
+
+            tempIP.append(1, tempLine[j]);
+        }
+        bigArray[i] = tempIP;
+    }
+
+    readFile.close();
+
+    for (int k = 0; k < size-1; ++k) {
+        for (int i = 1; i < size; ++i) {
+
+            if (bigArray[k] != bigArray[i])
+                size2++;
+        }
+    }
+
+    std::cout << size2;
+    //uniqueIP(bigArray, size);
+
+    return 0;
+}
+
+
+std::string* uniqueIP(std::string array[], int size) {
+
+}
+
+void ratio() {
+
+
+}
