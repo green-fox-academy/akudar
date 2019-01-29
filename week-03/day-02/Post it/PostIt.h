d7128f064110e10b872d24f362f8e1493e65f960
Adam Kudar <adam.kudar@gmail.com>
2019-01-29 14:01:13 +0100

    Post it

---
+++ week-03/day-02/Post it/PostIt.h
@@ -0,0 +1,18 @@
+#include <string>
+#ifndef POST_IT_POSTIT_H
+#define POST_IT_POSTIT_H
+
+
+class PostIt {
+
+public:
+
+    std::string text;
+    std::string textColor;
+    std::string backgroundColor;
+
+    PostIt(std::string txt, std::string tColor, std::string bColor);
+};
+
+
+#endif
