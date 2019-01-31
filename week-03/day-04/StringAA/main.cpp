4c79f204582a721d09b3d672f6a9f1fccea09134
Adam Kudar <adam.kudar@gmail.com>
2019-01-31 15:41:15 +0100

    StringA&A

--- week-03/day-04/StringAA/main.cpp
+++ week-03/day-04/StringAA/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+std::string aPlus(std::string input, int size) {
+
+    if (size - 1 == -1) {
+
+        return input;
+    } else {
+
+        input.insert(size-1, 1, 'a');
+        return aPlus(input, size - 1);
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << aPlus("sdfjnj", 6);
+
     return 0;
 }
